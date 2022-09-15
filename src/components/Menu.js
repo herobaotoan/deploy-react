c7cb35d51e890b839bee0d5164e03cd7b5235c71
Chi Toan <88176211+herobaotoan@users.noreply.github.com>
2022-09-15 14:27:17 +0700

    Update Menu.js

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,22 +1,28 @@
-import React, { useState, useEffect } from "react"
-
 export default function Menu() {
 
     return (
         <div>
-            <nav class="navbar navbar-default">
-                <div class="container-fluid">
-                    <div class="navbar-header">
-                        <a class="navbar-brand" href="#">PMS</a>
-                    </div>
-                    <ul class="nav navbar-nav">
-                        <li class="active"><a href="/">Home</a></li>
-                        <li><a href="/product">Product</a></li>
-                        <li><a href="/about">About</a></li>
-                    </ul>
+            <nav class="navbar navbar-expand-lg bg-dark text-white sticky-top">
+                <div class="col-auto">
+                    <img src="https://imgur.com/gfGufSO.png" alt="Home" style={{width:"140px",height:"40px"}}/>
+                </div>
+                |
+                <div class="col-1 text-center">
+                    <a href="/game" class="text-white">Main</a>
+                </div>
+                |
+                <div class="col-1 text-center">
+                    <a href="/login" class="text-white">Logout</a>
+                </div>
+                |
+                <div class="col-1 text-center">
+                    <a href="/top" class="text-white">Top Games</a>
+                </div>
+                |
+                <div class="col-1 text-center">
+                    <a href="/about" class="text-white">About</a>
                 </div>
             </nav>
         </div>
     )
-
 }
